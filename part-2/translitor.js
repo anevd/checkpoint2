70fac10e5dc2e63a71c52c8d382206a39ee89327
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-06-22 19:43:53 +0300

    add part-2

--- part-2/translitor.js
+++ part-2/translitor.js
@@ -3,7 +3,34 @@ const fs = require("fs").promises;
 const converter = require("./converter");
 
 class Translitor {
-  constructor() {}
+	constructor({ path }) {
+		this.path = path;
+		this.converter = converter;
+	}
+	async readFile(data) {
+		return await fs.readFile(`${this.path}/${data}`, "utf-8");
+	}
+	async convertString(data) {
+		let text = await this.readFile(data);
+		return text
+			.split("")
+			.map((el) => {
+				for (let [keys, values] of Object.entries(this.converter)) {
+					if (el === keys) {
+						return values;
+					}
+				}
+			})
+			.join("");
+	}
 }
 
 module.exports = Translitor;
+
+let translitor = new Translitor({
+	path: path.join(__dirname, "."),
+	converter,
+});
+
+// console.log(translitor.convertString("translitThis.txt"));
+translitor.convertString("translitThis.txt");
